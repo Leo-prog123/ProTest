7be08fd96d56028061c27edec71c2cf671bf531d
ThibaDaniil <122720119+ThibaDaniil@users.noreply.github.com>
2023-03-12 12:48:31 +0400

    Финальная версия теста с 10 вопросами.

--- 1/main.py
+++ 1/main.py
@@ -3,25 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "HARD STRING"
 
-
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
-@app.route('/obroz')
-def name():
-    return render_template('obroz.html')
-
-
-@app.route('/psiho')
-def psiho():
-    return render_template('psiho.html')
-
-
-@app.route('/razvl')
-def razvl():
-    return render_template('razvl.html')
+@app.route('/white')
+def white():
+    return render_template("Тест 3 вопроса.html")
 
 
 if __name__ == "__main__":
